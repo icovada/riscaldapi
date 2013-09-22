ffb7f359b09e4835a9c05112f5f5823086cb8eb7
Federico Tabbò <federico@tabbo.it>
2013-09-22 21:05:58 +0000

    added lib

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-$(document).ready(function(){
+$(function() {
+	$(".sparkline").sparkline('html', {
+		type: 'line',
+		width: '80%',
+    	height: '100',
+    	fillcolor: undefined,
+    	minSpotColor: '#0000ff',
+    	maxSpotColor: '#ff0000',
+    	spotRadius: 3,
+    	normalRangeMin: 19.7,
+    	normalRangeMax: 20.3,
+    	normalRangeColor: '#00ff00'
+    });
+});
 
-	$("#sparkline").sparkline([20,20.2,20.5,20.3,20,19.7,19.6,19.8,20 ], {
-    type: 'line',
-    width: '200',
-    height: '200',
-    fillColor: undefined,
-    spotColor: undefined,
-    minSpotColor: '#0000bf',
-    maxSpotColor: '#ff0000',
-    spotRadius: 3,
-    chartRangeMin: 19,
-    chartRangeMax: 21,
-    normalRangeMin: 19.7,
-    normalRangeMax: 20.3,
-    drawNormalOnTop: false});
-})
+function hideterm() {
+	$(".lcd").slideToggle();
+}
+
+$(document).ready(function() {
+    $(".lcd").on('click', hideterm);
+});
