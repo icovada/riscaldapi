a36f5c4fa1f562a752ae5b540f33fd2502c772f9
Federico Tabbò <federico@tabbo.it>
2013-10-08 11:51:14 +0200

    Added multiple schedule granularity

--- php/get.php
+++ php/get.php
@@ -1,11 +1,20 @@
 <?php
 $day = $_GET['day'];
+$precision = $_GET['prec';]
 $connection = mysqli_connect('localhost','riscaldapi','riscaldapi','riscaldapi')
         or die("Connection refused");
-    $query=("SELECT `temperature` FROM `schedule` WHERE `timestamp` >= '". 60*15*4*24*$day. "' AND `timestamp` < '". 60*15*4*24*($day+1)."'");
-    $result=mysqli_query($connection,$query);
-    while($row = mysqli_fetch_row($result)) {
-  		print($row[0].",");
-  	}
+
+    if (prec == '0') {
+	   	$query=("SELECT `temperature` FROM `schedulehourly` WHERE `timestamp` >= '". 60*15*4*24*$day. "' AND `timestamp` < '". 60*15*4*24*($day+1)."'");
+	    $result=mysqli_query($connection,$query);
+	    while($row = mysqli_fetch_row($result)) {
+	  		print($row[0].",");
+	  	}
+	} elseif (prec == '1') {
+	    $query=("SELECT `temperature` FROM `schedulehourly` WHERE `timestamp` >= '". 60*15*4*24*$day. "' AND `timestamp` < '". 60*15*4*24*($day+1)."'");
+	    $result=mysqli_query($connection,$query);
+	    while($row = mysqli_fetch_row($result)) {
+	  		print($row[0].",");
+	  	}
     mysqli_close($connection);
 ?>
