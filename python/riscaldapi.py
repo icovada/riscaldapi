8c8ccdd3015c06ff686089cd3198a3833b8324e4
Federico Tabbò <federico@tabbo.it>
2014-01-28 21:21:01 +0100

    Added function to guess if fire is on or not for logging. Run some code only if the right amount of values is returned

--- python/riscaldapi.py
+++ python/riscaldapi.py
@@ -14,6 +14,8 @@ time.sleep(1)
 conn = pymysql.connect(host="127.0.0.1", user="riscaldapi", passwd="riscaldapi", db="riscaldapi")
 cur = conn.cursor()
 
+flowOutHistory=[40,40,40,40]
+
 #out = open("out.csv","wb")
 while True:
 	try:
@@ -22,17 +24,31 @@ while True:
 		myin = test.readline()
 	except serial.serialutil.SerialException:
 		pass
-	temps = str(myin, encoding="ascii").strip().split(",")
-	print(temps)
-	try:
-		cur.execute("INSERT INTO `riscaldapi`.`temphistory` (`inside`, `flowIn`, `flowOut`, `goalTemp`, `diffTemp`, `rel1`) VALUES ('"+str(temps[0])+"', '"+str(temps[1])+"', '"+str(temps[2])+"', '"+str(temps[3])+"', '"+str(temps[4])+"', '"+str(temps[5])+"')");
-	except:
-		time.sleep(3)
 
-	try:
-		ret = rrdtool.update('riscaldapi.rrd', 'N:%f:%f:%f' %(float(temps[0]), float(temps[1]), float(temps[2])))
-	except:  #If the answer does not contain three fields (like an ACK)
-		pass
+	temps = str(myin, encoding="ascii").strip().split(",")
+
+	if (len(temps) == 7):
+		#Guess whether fire is on or not
+		flowOut = float(temps[2])
+		if (flowOut > (sum(flowOutHistory) / len(flowOutHistory) )):
+			fire = 1
+		else:
+			fire = 0
+
+		#Keep the last 4 values for average
+		flowOutHistory.append(float(temps[2]))
+		del flowOutHistory[0]
+
+		print(temps, fire)
+		try:
+			cur.execute("INSERT INTO `riscaldapi`.`temphistory` (`inside`, `flowIn`, `flowOut`, `goalTemp`, `diffTemp`, `rel1`, `fire`) VALUES ('"+str(temps[0])+"', '"+str(temps[1])+"', '"+str(temps[2])+"', '"+str(temps[3])+"', '"+str(temps[4])+"', '"+str(temps[5])+"', '"+str(fire)+"')");
+		except:
+			time.sleep(3)
+
+		try:
+			ret = rrdtool.update('riscaldapi.rrd', 'N:%f:%f:%f' %(float(temps[0]), float(temps[1]), float(temps[2])))
+		except:  #If the answer does not contain three fields (like an ACK)
+			pass
 
 	time.sleep(4)
 	conn.commit()
